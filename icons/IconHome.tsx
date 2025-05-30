d5ed312d0b65a1259a518fc211412a2fca4fd763
5ive30 <5ive30@users.noreply.github.com>
2025-05-30 09:50:59 +0900

    Add files via upload

---
+++ icons/IconHome.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export const IconHome = () => (
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M3 9.5L12 3l9 6.5V21a1 1 0 0 1-1 1h-5v-6H9v6H4a1 1 0 0 1-1-1V9.5z" fill="#000"/>
+  </svg>
+);

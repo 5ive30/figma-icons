37627d1df0f5a58b95655680e631ef8973bae279
5ive30 <5ive30@users.noreply.github.com>
2025-05-30 11:38:42 +0900

    upload iconinfooutline from Reacticon

---
+++ icons/iconinfooutline.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const iconinfooutline = () => (
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M2 10C2 5.58 5.58 2 10 2C14.42 2 18 5.58 18 10C18 14.42 14.42 18 10 18C5.58 18 2 14.42 2 10ZM10 16.2C6.58225 16.2 3.8 13.4177 3.8 10C3.8 6.58225 6.58225 3.8 10 3.8C13.4178 3.8 16.2 6.58225 16.2 10C16.2 13.4177 13.4178 16.2 10 16.2Z" fill="#222222"/>
+<path d="M10.8 14H9.2V9.2H10.8V14Z" fill="#222222"/>
+<path d="M10.8 7.6H9.2V6H10.8V7.6Z" fill="#222222"/>
+</svg>
+
+);

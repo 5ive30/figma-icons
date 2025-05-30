d924bb1739a748238b7441356819984842a54f7f
5ive30 <5ive30@users.noreply.github.com>
2025-05-30 13:53:16 +0900

    feat(icon): Add iconerrorcircle

---
+++ icons/iconerrorcircle.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const iconerrorcircle = () => (
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10 2C5.584 2 2 5.584 2 10C2 14.416 5.584 18 10 18C14.416 18 18 14.416 18 10C18 5.584 14.416 2 10 2Z" fill="#DE3730"/>
+<path d="M9.20001 14H10.8V12.4H9.20001V14Z" fill="white"/>
+<path d="M9.20001 10.8H10.8V6H9.20001V10.8Z" fill="white"/>
+</svg>
+
+);
